88653938d58e016e941d8a95005f52d9f8eb1b00
daehwan <daehwan5024@gmail.com>
2024-07-16 18:44:53 +0900

    2024-07-16 기숙사

--- createData.m
+++ createData.m
@@ -16,7 +16,7 @@ function parsave(fname, a, b, c, d)
   save(fname, 'a', 'b', 'c', 'd')
 end
 tic
-parfor k=1:100
+parfor k=1:10
     num_radar = 8;
     posAbsolute = rand(3, num_radar);
     posAbsolute(1:2, :) = posAbsolute(1:2, :) * 20 - 10;
